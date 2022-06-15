b01ab09e94833905329a5a443a3894c48a638cc6
Aline Alves <48930961+afsalves@users.noreply.github.com>
2022-06-15 20:49:25 -0300

    Creating screens basic structure

---
+++ src/modules/Favorites/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { SafeAreaView, ScrollView, Text } from 'react-native';
+
+
+
+const Favorites: React.FC = () => {
+  return (
+    <SafeAreaView >
+      <ScrollView>
+        <Text>'Favoritos'</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Favorites;
---
+++ src/modules/Home/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { SafeAreaView, ScrollView, Text } from 'react-native';
+
+
+
+const Home: React.FC = () => {
+  return (
+    <SafeAreaView >
+      <ScrollView>
+        <Text>'Home'</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
