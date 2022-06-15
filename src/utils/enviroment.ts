8af614fe4e2bdfb1a07a3e7546a9d53cc54f4fd2
Aline Alves <48930961+afsalves@users.noreply.github.com>
2022-06-14 23:21:21 -0300

    Creating app enviroments

---
+++ src/utils/enviroment.ts
@@ -0,0 +1,12 @@
+
+
+const appEnvironments = {
+    apiKey: '1362f935116cf25304b335d1765ae903',
+
+    apiToken:'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMzYyZjkzNTExNmNmMjUzMDRiMzM1ZDE3NjVhZTkwMyIsInN1YiI6IjYyYTkyZWIxN2UxMmYwMGUyODM2YjM4OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dRAz9vJUis5e0yJMzwhC12BwwfbMA8sqN69pAd2D7Hc'
+
+}
+
+
+
+export default appEnvironments
