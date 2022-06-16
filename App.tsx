4afbb4b1ec92758ace9276855ac06e2b6dcbc369
Aline Alves <48930961+afsalves@users.noreply.github.com>
2022-06-16 09:27:51 -0300

    Adjusting navigation

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Routes from "./src/routes";
+import Routes from "./src/routes/routes";
 import {LastestMoviesProvider} from '@hooks/useLastestMovies'
 
 export default function App() {
---
+++ src/routes/bottom.routes.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Home from '@modules/Home'
+import Favorites from '@modules/Favorites'
+import { MaterialIcons, Ionicons } from '@expo/vector-icons';
+
+const {Navigator, Screen} = createBottomTabNavigator();
+
+const BottomNavigation: React.FC = () => {
+  return (
+    <Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        tabBarActiveTintColor: 'red',
+        headerShown: false,
+        tabBarShowLabel: true,
+        tabBarLabelStyle: {color: 'white', fontSize: 12},
+        tabBarStyle: {
+          backgroundColor: 'black',
+          height: 100,
+          borderTopColor: 'white',
+          borderTopWidth: 0.5,
+        },
+      }}
+    >
+      <Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: (({size, focused}) => (
+            <Ionicons
+              name="home-outline"
+              size={size}
+              color={focused ? 'red' : 'white'}
+            />
+          ))
+        }}
+      />
+
+      <Screen
+        name="Favorites"
+        component={Favorites}
+        options={{
+          tabBarIcon: (({size, focused}) => (
+            <MaterialIcons
+              name="favorite-outline"
+              size={size}
+              color={focused ? 'red' : 'white'}
+            />
+          ))
+        }}
+      />
+    </Navigator>
+  );
+};
+
+export default BottomNavigation;
