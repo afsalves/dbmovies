c8e9b922b46bc3a101cfd6cfe760f5c77ac08f9a
Aline Alves <48930961+afsalves@users.noreply.github.com>
2022-06-16 10:46:48 -0300

    Adding fonts configuration

--- App.tsx
+++ App.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import Routes from "./src/routes/routes";
 import {LastestMoviesProvider} from '@hooks/useLastestMovies'
+import AppLoading from 'expo-app-loading';
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold
+} from '@expo-google-fonts/jost';
 
 export default function App() {
+  const [ fontsLoaded ] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  });
+
+  if(!fontsLoaded)
+  return <AppLoading />
 
   return (
     <LastestMoviesProvider>
