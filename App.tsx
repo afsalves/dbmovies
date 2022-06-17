36067fdcd4602a3bdfc4a81ba30f1a35cbaa669e
Aline Alves <48930961+afsalves@users.noreply.github.com>
2022-06-17 02:37:27 -0300

    Develop (#1)
    * Adjusting paths configuration
    
    * Creating axios instance of api
    
    * Creating useLastestMovies hook
    
    * Creating app enviroments
    
    * Adding navigation dependencies and clean script
    
    * Adding jsx compiler options
    
    * Adding navigation icons
    
    * Creating screens basic structure
    
    * Adjusting routes and app.tsx
    
    * Revert "Creating app enviroments"
    
    This reverts commit 8af614fe4e2bdfb1a07a3e7546a9d53cc54f4fd2.
    
    * Adjusting navigation
    
    * Adjusting dependencies and paths
    
    * Adding fonts configuration
    
    * Adding svg declare module
    
    * Adding global fonts and colors
    
    * Adjusting paths
    
    * Adjusting gitignore
    
    * Adding expo linear gradient
    
    * Adding svg configurations and lib
    
    * Adding movie icon svg
    
    * Creating background component
    
    * Creating header component
    
    * Adjusting theme
    
    * Adjusting importation
    
    * Adjusting home and bottom tab layout
    
    * Adjusting movie api
    
    * Adding swiper flat list lib
    
    * Adding .env on gitignore
    
    * Refactoring hook useDBMovies
    
    * Adding star icon
    
    * Creating card movie component
    
    * Creating horizontal carousel component
    
    * Adjusting header component
    
    * Adjusting theme colors
    
    * Adding horizontal carousel on home
    
    * Adding movie detail screen
    
    * Refactoring hooks and api calls
    
    * Adding and excluding icons
    
    * Adjusting card movies
    
    * Adjusting header
    
    * Adjusting carousel
    
    * Removing configs importations
    
    * Adjusting favorites
    
    * Adjusting home
    
    * Doing movie detail page
    
    * Adding generes utils
    
    * Adding readme
    
    * Update README.md

--- App.tsx
+++ App.tsx
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import Routes from "./src/routes/routes";
+import {DBMoviesProvider} from '@hooks/useDBMovies'
+import AppLoading from 'expo-app-loading';
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold
+} from '@expo-google-fonts/jost';
 
 export default function App() {
+  const [ fontsLoaded ] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  });
+
+  if(!fontsLoaded)
+  return <AppLoading />
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <DBMoviesProvider>
+      < Routes />
+    </DBMoviesProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
